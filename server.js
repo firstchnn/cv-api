227ce666280f53e1b5a8464d72b212bc9b397030
chnw.dev <cnw.first@gmail.com>
2022-06-08 08:25:08 +0700

    update view pdf

--- server.js
+++ server.js
@@ -7,22 +7,28 @@ const { ObjectId, Int32, Decimal128, Binary } = require('bson');
 const { stringify } = require('querystring');
 const port = process.env.PORT || 4000;
 const cors = require('cors');
-
-
-//define storage for pdf files
-
-// const storage = multer.diskStorage({
-//     destination: function (request, file, callback) {
-//         callback(nill, '')
-//     }
-// })
+const path = require('path');
+const multer = require('multer')
+const crypto = require('crypto');
+const GridFsStorage = require('multer-gridfs-storage').GridFsStorage;
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const mongodb = require('mongodb');
+const binary = mongodb.Binary;
+const mongoClient = mongodb.MongoClient
 
 app.use(cors());
+app.use(fileUpload());
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(multer);
 app.set('view engine', 'ejs');
 
+const mongoURI = "mongodb+srv://chnw-admin:chnw1234@cluster0.8ckv3.mongodb.net/applicantDB";
+
 mongoose.connect("mongodb+srv://chnw-admin:chnw1234@cluster0.8ckv3.mongodb.net/applicantDB", {useNewUrlParser: true},{useUnifiedTopology: true})
+
 
 //create data schema
 const applicantSchema = {
@@ -76,9 +82,13 @@ app.get("/all-cv", function(req, res) {
     })
 })
 
-app.post("/", function(req, res) {
-    let newApplicant = new Applicant({
-        name: req.body.name,
+
+app.get("/download", function(req,res) {
+    getFiles(req);
+})
+
+app.post("/upload", function(req, res) {
+    let file = { name: req.body.name,
         totalExp : req.body.exp,
         gpa : req.body.gpa,
         majorSkill : req.body.majorSkill,
@@ -91,35 +101,60 @@ app.post("/", function(req, res) {
         interviewDate : req.body.interviewDate,
         startDate : req.body.startDate,
         status : req.body.status,
-        CV : req.body.cv,
-    });
-
-    let newMajorSkills = new MajorSkills({
-        name : req.body.majorSkill,
-        totalExp : req.body.majorExp,
-        applicantName : req.body.name,
-    })
-
-    let newMinorSkills = new MinorSkills({
-        name : req.body.minorSkill,
-        totalExp : req.body.minorExp,
-        applicantName : req.body.name,
-    })
-
-    let newLanguageSkills = new LanguageSkills({
-        name : req.body.langSkill,
-        proficiency : req.body.proficiency,
-        applicantName : req.body.name,
-    })
-    newApplicant.save();
-    newMajorSkills.save();
-    newMinorSkills.save();
-    newLanguageSkills.save();
+        file : binary(req.files.uploadedFile.data), }
+    insertFile(file, res)
     res.redirect('https://cv-frontend-bb249.web.app/');
 })
+
+async function insertFile(file, res) {
+    await mongoClient.connect('mongodb+srv://chnw-admin:chnw1234@cluster0.8ckv3.mongodb.net/applicantDB', { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            return err
+        }
+        else {
+            let db = client.db('applicantDB')
+            let collection = db.collection('applicants')
+            try {
+                collection.insertOne(file)
+                console.log('File Inserted')
+            }
+            catch (err) {
+                console.log('Error while inserting:', err)
+            }
+            // client.close()
+            // res.redirect('/')
+        }
+
+    })
+}
+
+async function getFiles(name) {
+    await mongoClient.connect('mongodb+srv://chnw-admin:chnw1234@cluster0.8ckv3.mongodb.net/applicantDB', { useNewUrlParser: true }, (err, client) => {
+        if (err) {
+            return err
+        }
+        else {
+            let db = client.db('applicantDB')
+            let collection = db.collection('applicants')
+            collection.find({name: name}).toArray((err, doc) => {
+                if (err) {
+                    console.log('err in finding doc:', err)
+                }
+                else {
+                    let buffer = doc[0].file.buffer
+                    console.log('found buffer:', buffer)
+                    fs.writeFileSync('file.pdf', buffer)
+                }
+            })
+            // client.close()
+        }
+
+    })
+}
 
 app.listen(port, function() {
     console.log(`server is running on ${port}`);
 })
+
 
 module.exports = {app};
